419c4d2796506874ba2582be5fc24c51c9c80ab3
Kerem AR <keremar0000@gmail.com>
2025-09-17 12:06:10 +0300

    hotfix

--- vars/deployToProductionWithKustomize.groovy
+++ vars/deployToProductionWithKustomize.groovy
@@ -54,6 +54,7 @@ def call(Map config) {
     container('docker') {
         echo "🔧 Installing Kustomize..."
         sh '''
+            apk add --no-cache curl bash
             curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
             mv kustomize /usr/local/bin/
         '''
--- vars/deployWithKustomize.groovy
+++ vars/deployWithKustomize.groovy
@@ -17,6 +17,7 @@ def call(Map config) {
     container('docker') {
         echo "🔧 Installing kubectl..."
         sh '''
+            apk add --no-cache curl
             curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
             chmod +x kubectl
             mv kubectl /usr/local/bin/
